a4d09d3e4d5fb179a34318c5be20b23e37d5e13f
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2022-06-29 19:39:48 -0400

    Geolocation Support
    - Add support for geolocation detection to avoid manually entering latitude and longitude
    - Change Latitude and Longitude inputs to controlled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState, useEffect } from "react";
 import CoordinateForm from "./CoordinateForm";
 import axios from "axios";
 import Forecast from "./Forecast";
@@ -7,16 +7,14 @@ import { Row } from "react-bootstrap";
 
 function App() {
 	const [forecast, setForecast] = useState([]);
-	const latitudeRef = useRef();
-	const longitudeRef = useRef();
+	const [latitude, setLatitude] = useState("");
+	const [longitude, setLongitude] = useState("");
 
 	// Makes REST call to get the office and grid points
 	const submitCoordinates = async (event) => {
 		event.preventDefault();
-		const lat = latitudeRef.current.value;
-		const long = longitudeRef.current.value;
 
-		const url = `https://api.weather.gov/points/${lat},${long}`;
+		const url = `https://api.weather.gov/points/${latitude},${longitude}`;
 		let response = await axios({
 			method: "GET",
 			url: url,
@@ -38,14 +36,25 @@ function App() {
 		setForecast(response.data.properties.periods);
 	};
 
+	// Used to detect the user's location and set the Latitude and Longitude states
+	const getLocation = () => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			setLatitude(position.coords.latitude);
+			setLongitude(position.coords.longitude);
+		});
+	};
+
 	return (
 		<Container className="text-center">
 			<Row className="mt-4">
 				{/* Renders the Coordinate input elements */}
 				<CoordinateForm
 					onSubmit={submitCoordinates}
-					latitudeRef={latitudeRef}
-					longitudeRef={longitudeRef}
+					latitude={latitude}
+					longitude={longitude}
+					getLocation={getLocation}
+					setLatitude={setLatitude}
+					setLongitude={setLongitude}
 				/>
 			</Row>
 
@@ -56,5 +65,4 @@ function App() {
 		</Container>
 	);
 }
-
 export default App;
--- src/CoordinateForm.js
+++ src/CoordinateForm.js
@@ -3,10 +3,21 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 // Buids the input fields for entering coordinates
 // Returns Refs to the <App /> component to process
 const CoordinateForm = (props) => {
+	// Maintains control of latitude input
+	const handleLatitudeChange = (event) => {
+		props.setLatitude(event.target.value);
+	};
+
+	// Maintains control of longitude input
+	const handleLongitudeChange = (event) => {
+		props.setLongitude(event.target.value);
+	};
+
 	return (
 		<Form onSubmit={props.onSubmit}>
 			<Container>
@@ -15,24 +26,35 @@ const CoordinateForm = (props) => {
 						<Form.Control
 							type="text"
 							placeholder="Enter latitude"
-							ref={props.latitudeRef}
+							onChange={handleLatitudeChange}
+							value={props.latitude}
 						/>
 					</Col>
 					<Col sm={12} md={6} lg={5} className="my-2 my-md-0">
 						<Form.Control
 							type="text"
 							placeholder="Enter longitude"
-							ref={props.longitudeRef}
+							onChange={handleLongitudeChange}
+							value={props.longitude}
 						/>
 					</Col>
 					<Col sm={12} lg={2} className="mt-md-2 mt-lg-0">
-						<Button
-							variant="primary"
-							type="submit"
-							className="w-100"
-						>
-							Submit
-						</Button>
+						<ButtonGroup className="w-100">
+							<Button
+								variant="warning"
+								onClick={props.getLocation}
+							>
+								<i className="bi bi-geo-alt"></i>
+							</Button>
+
+							<Button
+								variant="primary"
+								type="submit"
+								className="w-100"
+							>
+								Submit
+							</Button>
+						</ButtonGroup>
 					</Col>
 				</Row>
 			</Container>
